307a907408dcff1c924a0c78d91b3ed63ff86691
hanaasal <hana_asal@aucegypt.edu>
2020-04-19 23:09:52 +0200

    Hana: added sh,sb,jalr, jal (not working yet)

--- vivado/exp1.srcs/sources_1/new/controlUnit.v
+++ vivado/exp1.srcs/sources_1/new/controlUnit.v
@@ -11,18 +11,22 @@ module controlUnit
     output reg [1:0]ALUop,
     output reg memwrite,
     output reg ALUsrc,
-    output reg regwrite
-   
+    output reg regwrite,
+    output reg JUMP
     );
     
     
     always@(*)begin
         case (inst[6:2])
-            5'b01100: {branch,memread,mem2reg,ALUop[1:0],memwrite,ALUsrc,regwrite} = 8'b00010001;   //r
-            5'b00000: {branch,memread,mem2reg,ALUop[1:0],memwrite,ALUsrc,regwrite} = 8'b01100011;   //lw
-            5'b01000: {branch,memread,mem2reg,ALUop[1:0],memwrite,ALUsrc,regwrite} = 8'b00000110;   //sw
-            5'b11000: {branch,memread,mem2reg,ALUop[1:0],memwrite,ALUsrc,regwrite} = 8'b10001000;   //beq  
-            5'b00100: {branch,memread,mem2reg,ALUop[1:0],memwrite,ALUsrc,regwrite} = 8'b00000011;   //I format (arith)  
+            5'b01100: {branch,memread,mem2reg,ALUop[1:0],memwrite,ALUsrc,regwrite,JUMP} = 9'b000100010;   //r
+            5'b00000: {branch,memread,mem2reg,ALUop[1:0],memwrite,ALUsrc,regwrite,JUMP} = 9'b011000110;   //lw
+            5'b01000: {branch,memread,mem2reg,ALUop[1:0],memwrite,ALUsrc,regwrite,JUMP} = 9'b000001100;   //sw
+            5'b11000: {branch,memread,mem2reg,ALUop[1:0],memwrite,ALUsrc,regwrite,JUMP} = 9'b100010000;   //beq  
+            5'b00100: {branch,memread,mem2reg,ALUop[1:0],memwrite,ALUsrc,regwrite,JUMP} = 9'b000000110;   //I format (arith)  
+            5'b11001: {branch,memread,mem2reg,ALUop[1:0],memwrite,ALUsrc,regwrite,JUMP} = 9'b000000111;   //I format (JALR)  
+            //5'b11011: {branch,memread,mem2reg,ALUop[1:0],memwrite,ALUsrc,regwrite,JUMP} = 9'b100000111;   //UJ format (JAL)
+            5'b11011: {branch,memread,mem2reg,ALUop[1:0],memwrite,ALUsrc,regwrite,JUMP} = 9'b100010111;   //UJ format (JAL)  
+
         endcase
         
     end
